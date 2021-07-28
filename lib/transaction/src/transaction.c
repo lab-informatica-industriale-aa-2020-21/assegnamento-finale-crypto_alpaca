378c017d0344e982633686cb4c13102841a0d2bd
Filippo Toffano <filippo.toffano@studenti.unipd.it>
2021-07-28 17:25:34 +0200

    aggiunti commenti al codice implementato. Correzione di errori trovati durante la revisione del codice

--- lib/transaction/src/transaction.c
+++ lib/transaction/src/transaction.c
@@ -11,11 +11,11 @@ trans *head = NULL;
 
 // Funzione per la creazione di una nuova transazione: 
 
-trans *new_trans(int32_t public_key_sender, int32_t public_key_receiver, int32_t amount_transaction, int &count ){
-    trans *tmp_transaction;
-    tmp_transaction = malloc(sizeof(trans));
+trans *new_trans(const int32_t public_key_sender, const int32_t public_key_receiver, const int32_t amount_transaction, int *const count ){
+    trans *tmp_transaction;                     // definizione di una variabile con struttura 'trans'
+    tmp_transaction = malloc(sizeof(trans));    // allocazione di memoria per una transazione
 
-    // Controllo funzioanmento malloc():
+    // Controllo funzioanmento corretto di malloc():
     if(tmp_transaction == NULL){
         printf("Error: malloc() failed");
         exit(EXIT_FAILURE)
@@ -25,8 +25,9 @@ trans *new_trans(int32_t public_key_sender, int32_t public_key_receiver, int32_t
     tmp_transaction -> sender = public_key_sender;            // inserimento della chiave pubblica del mittente 
     tmp_transaction -> receiver = public_key_receiver;        // inserimenro della chiave pubblica del ricevente 
     tmp_transaction -> amount = amount_transaction;           // inserimento dell'importo da trasferire nella transazione
-    tmp_transaction -> next = NULL;
+    tmp_transaction -> next = NULL;                           // puntatore al successivo della lista 
 
+    // incremento del contatore che tiene traccia del numero di transazioni che vengono create:
     *count ++;
 
     return tmp_transaction;
@@ -34,10 +35,10 @@ trans *new_trans(int32_t public_key_sender, int32_t public_key_receiver, int32_t
 
 /* Creazione della lista di transazioni:
     questa funzione aggiunge le nuove transazioni alla lista delle transazioni  */
-void add_trans(const uint32_t sender,const uint32_t receiver, const uint32_t amount, trans *head, int &count){
-    trans *old_head = head;
-    head = new_trans(int32_t public_key_sender, int32_t public_key_receiver, int32_t amount_transaction, int &count );
-    old_head -> next = head;
+void add_trans(const uint32_t sender,const uint32_t receiver, const uint32_t amount, trans *head, int *const count){
+    trans *old_head = head;                                         // salvataggio della 'testa' della lista 
+    head = new_trans(sender, receiver, transaction, &count);        // assegnazione della nuova 'testa' della lista  
+    old_head -> next = head;                                        // assegnazione del puntatore 'next' della transazione precedente
 }
 
 // Funzione che ritorna il numero delle transazioni che sono state create:
