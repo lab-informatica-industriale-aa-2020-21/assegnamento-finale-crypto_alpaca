62469ab2b67ee468664830bb46255b4ffb15556e
Filippo Toffano <filippo.toffano@studenti.unipd.it>
2021-07-30 12:04:53 +0200

    Implementata la funzione per la creazione della blockchain

--- lib/chain/src/chain.c
+++ lib/chain/src/chain.c
@@ -1,13 +1,33 @@
 // File.c per la creazione di una nuova blockchain:
 
 #include "block.h"
+#include "chain.h"
+#include<stdint.h>
 
-block *head_block = NULL;
+/*  Funzione per la creazione di una nuova blockchain:
+    Viene passata il puntatore alla lista delle transazioni che devono essere inserire nel blocco;
+    Viene creato un nuovo blocco richiamando la funzione 'new_block';
+    Successivamente il blocco viene minato attraverso l'esecuzione della funzione 'mine';
+*/
+void add_block(block *head_block, trans *head_trans){
+    
+    // Creazione del blocco:
+    new_block(const block *head_block, const trans *first_trans);
+    
+    // Il blocco creato viene minato 
+    mine(block *const block_to_mine, uint *const count_index);
 
-// Aggiunta di un blocco minato alla blockchain:
-void add_block(block *last, trans *head){
-    block *old_head_block = head_block;
-    head = mine(block *block_to_mine, uint index);
-    old_head_block -> head = head_block;
+}
+
+/*  Creazione di una nuova blockchain (di una catena )
+    Avendo più linee di 'catene' (blockchain) è necessaria una funzione che ne crei una nuova*/ 
+chain *new_chain(const chain *head_chain, const block )
+
+block *new_block(const block *head_block){
+
+    *head_block = NULL;
+
+    // Richiamo della funzione per aggiungere un nuovo blocco alla catena:
+    add_block(block *head_block, trans *head_trans);
 }
 
