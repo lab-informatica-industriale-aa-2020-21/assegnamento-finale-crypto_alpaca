341774c3653487edc315a1a8fb9ee1adb40b2d0a
Alberto Trabacchin <alberto.trabacchin@studenti.unipd.it>
2021-08-02 11:46:37 +0200

    Aggiornata funzione test di sigma_0 con uint_32)

--- test/test_hash/src/test_hash.c
+++ test/test_hash/src/test_hash.c
@@ -9,7 +9,7 @@ void tearDown(void) {}
 
 //Operations: y_test = rot(x,7) XOR rot(x,18) XOR shift_dx(x,3)
 void test_sigma_0_shouldCalculateSigma0OperationSeriesTox(void) {
-	TEST_ASSERT_EQUAL_HEX(sigma_0(0x4AF), 0x5F2BC09C);
+	TEST_ASSERT_EQUAL_UINT32(2629839711, sigma_0(44804));
 }
 
 int main(void) {
