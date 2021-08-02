bd1074ac01ba2019e89b29dd64f5524f994d5536
Filippo Toffano <filippo.toffano@studenti.unipd.it>
2021-08-02 11:40:40 +0200

    Iniziata implementazione del main.c; definzione delle librerie utili per il progetto (hash.h, trans.h, librerie standard, ecc...)

---
+++ app/src/main.c
@@ -0,0 +1,20 @@
+/*  Programma 'main' in cui viene implementato il programma principale di gestione 
+    dell'algoritmo di blockchain.
+    In questa parte di software vengono richiamate le sottoparti sviluppate (chain, transaction, block, hash).
+*/
+
+// Definizione delle librerie utili per il progetto 
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+#include <stdint.h>
+#include "hash.h"
+#include "trans.h"
+#include "block.h"
+#include "chain.h"
+
+
+int main()
+{
+
+}
