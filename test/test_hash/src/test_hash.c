0613f056a112d6dc078562901024eec3385e1280
Alberto Trabacchin <alberto.trabacchin@studenti.unipd.it>
2021-08-02 11:38:53 +0200

    Aggiunta funzione di test di sigma_0

--- test/test_hash/src/test_hash.c
+++ test/test_hash/src/test_hash.c
@@ -6,10 +6,16 @@ void setUp(void) {}
 
 void tearDown(void) {}
 
+
+//Operations: y_test = rot(x,7) XOR rot(x,18) XOR shift_dx(x,3)
+void test_sigma_0_shouldCalculateSigma0OperationSeriesTox(void) {
+	TEST_ASSERT_EQUAL_HEX(sigma_0(0x4AF), 0x5F2BC09C);
+}
+
 int main(void) {
 
 	UNITY_BEGIN();
-
+	RUN_TEST(test_sigma_0_shouldCalculateSigma0OperationSeriesTox);
 	return(UNITY_END());
 
 }
