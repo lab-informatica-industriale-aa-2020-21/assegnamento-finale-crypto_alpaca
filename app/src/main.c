91a6679c105431c4492f0063bab2287dc3599c04
Alberto Trabacchin <alberto.trabacchin@studenti.unipd.it>
2021-08-03 16:04:21 +0200

    Correzione errore sintassi nel main

--- app/src/main.c
+++ app/src/main.c
@@ -14,9 +14,9 @@
 
 int main()
 {
-    printf("Benvenuto! \n 
-            Questo programma simula l'algoritmo di gestione di una blockchain
-            Sei pronto a scoprirlo? Premi un tasto per continuare... ");
+    printf("Benvenuto! \n"
+           "Questo programma simula l'algoritmo di gestione di una blockchain"
+           "Sei pronto a scoprirlo? Premi un tasto per continuare... ");
     
     getchar(); // attesa di un input da tastiera per continuare l'esecuzione del programma
 
