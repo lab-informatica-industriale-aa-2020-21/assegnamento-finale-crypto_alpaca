00fa049aaa4b39cab507efc44b1c259dd849aeb9
Alberto Trabacchin <alberto.trabacchin@studenti.unipd.it>
2021-08-03 10:37:02 +0200

    Correzione errori sintassi nelle funzioni di chain.c

--- lib/chain/src/chain.c
+++ lib/chain/src/chain.c
@@ -2,20 +2,20 @@
 
 #include "block.h"
 #include "chain.h"
-#include<stdint.h>
+#include <stdint.h>
 
 /*  Funzione per la creazione di una nuova blockchain:
     Viene passata il puntatore alla lista delle transazioni che devono essere inserire nel blocco;
     Viene creato un nuovo blocco richiamando la funzione 'new_block';
     Successivamente il blocco viene minato attraverso l'esecuzione della funzione 'mine';
 */
-void add_block(block *head_block, trans *head_trans){
+void add_block(block *const head_block, const trans *head_trans){
     
     // Creazione del blocco:
-    new_block(block *const head_block, const trans *first_trans);
+    new_block(head_block, first_trans);
     
     // Il blocco creato viene minato 
-    mine(block *const block_to_mine, uint *const count_index);
+    mine(block_to_mine, count_index);
 
 }
 
