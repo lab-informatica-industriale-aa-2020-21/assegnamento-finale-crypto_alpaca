9aaff04dd6d6e3d80934c978f621a1e4c7ac9c26
Filippo Toffano <filippo.toffano@studenti.unipd.it>
2021-08-04 14:27:51 +0200

    Aggiornamento completo delle cartelle

---
+++ build/app/CMakeFiles/application.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/application.dir/src/main.c.o"
+  "application"
+  "application.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/application.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/lib/transaction/CMakeFiles/transaction.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/filippotoffano/lab-informatica-industriale20-21/assegnamento-finale-crypto_alpaca/lib/transaction/src/transaction.c" "/home/filippotoffano/lab-informatica-industriale20-21/assegnamento-finale-crypto_alpaca/build/lib/transaction/CMakeFiles/transaction.dir/src/transaction.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../lib/transaction/include"
+  "../lib/hash/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/filippotoffano/lab-informatica-industriale20-21/assegnamento-finale-crypto_alpaca/build/lib/hash/CMakeFiles/hash.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
