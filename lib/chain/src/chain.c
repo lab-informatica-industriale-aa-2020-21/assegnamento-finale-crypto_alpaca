4509165a8689fefaf08e0e5c25b99b9620ba4a99
Filippo Toffano <filippo.toffano@studenti.unipd.it>
2021-08-04 16:18:01 +0200

    Aggiunti commenti di descrizione delle funzioni e delle variabile che vengono passate; eliminata la funzione 'new_block' inutile (già contenuta in block.c)

--- lib/chain/src/chain.c
+++ lib/chain/src/chain.c
@@ -5,34 +5,40 @@
 #include "transaction.h"
 #include <stdint.h>
 
-/*  Funzione per la creazione di una nuova blockchain:
-    Viene passata il puntatore alla lista delle transazioni che devono essere inserire nel blocco;
-    Viene creato un nuovo blocco richiamando la funzione 'new_block';
-    Successivamente il blocco viene minato attraverso l'esecuzione della funzione 'mine';
+/*Funzione: add_block
+* ------------------------------------------------------------------------------------------------------
+* Viene passata il puntatore alla lista delle transazioni che devono essere inserire nel blocco;
+* Viene creato un nuovo blocco richiamando la funzione 'new_block';
+* Successivamente il blocco viene minato attraverso l'esecuzione della funzione 'mine';
+* ------------------------------------------------------------------------------------------------------
+*  
+* *head_block: puntatore al blocco di testa (ultimo blocco aggiunto)
+* *head_trans: puntatore alla transazione di testa (ultima transazione creata)
+*
 */
 void add_block(block *const head_block, const trans *head_trans){
     
     // Creazione del blocco:
     block *block_to_mine =  new_block(head_block, head_block -> first_trans);
     
-    // Il blocco creato viene minato 
+    // Il blocco creato viene minato (operazione di mining) 
     mine(block_to_mine);
 
 }
 
-/*  Creazione di una nuova blockchain (di una catena )
-    Avendo più linee di 'catene' (blockchain) è necessaria una funzione che ne crei una nuova*/ 
+/* Funzione: new_chain
+*---------------------------------------------------------------------------------------------------------------
+* Creazione di una nuova blockchain.
+* Con questa funzione è possibile creare più linee di chain
+*---------------------------------------------------------------------------------------------------------------
+*
+* *new_chain: puntatore alla testa della chain (ultima catena creata)
+* 
+*/
 void *new_chain(chain *head_chain)
 {
-    head_chain -> head_block = NULL;
+    head_chain -> head_block = NULL;        // spostamento della testa della catena
     // gestione della testa della head_chain (aggiungere)
 }
 
-// Funzione per la creazione di un nuovo blocco della catena:
-block *new_block(block *const head_block, const trans *head_trans)
-{
-
-    // Richiamo della funzione per aggiungere un nuovo blocco alla catena:
-    add_block(head_block, head_trans);
-}
 
