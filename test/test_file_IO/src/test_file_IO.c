ebe845172ad882dcf12897d022a4f51c35d96f94
Alberto Trabacchin <alberto.trabacchin@studenti.unipd.it>
2021-08-04 16:52:17 +0200

    Aggiunta funzione di test di print_block_trans

--- test/test_file_IO/src/test_file_IO.c
+++ test/test_file_IO/src/test_file_IO.c
@@ -84,6 +84,74 @@ void test_print_trans_ShouldFormatFixedLenStringWithSingleTransInfo(void) {
 
 }
 
+void test_print_block_trans_ShouldFormatFixedLenStringWithAllBlockTransInfo(void) {
+	char test_string[TRANS_LENGTH * 3 + LINE_LENGTH + 1];
+	
+	time_t creation_time = time(NULL);
+
+	block test_block = {
+		25,												//count_index
+		NULL,											//prev_hash
+		{213, 19232, 0, 191302, 1752, 1, 5, 12332},		//hash
+		39291,											//nonce
+		NULL,											//*first_trans
+		3,												//num_trans
+		creation_time,
+		NULL											//head_block
+	};
+
+	trans test_trans1 = {
+		23913,
+		9138432,
+		1340,
+		NULL,
+		NULL,
+		1
+	};
+
+	trans test_trans2 = {
+		192323,
+		1039,
+		10923,
+		NULL,
+		NULL,
+		2
+	};
+
+	trans test_trans3 = {
+		12321,
+		9834,
+		10,
+		NULL,
+		NULL,
+		3
+	};
+
+	test_block.first_trans = &test_trans1;
+	test_trans1.next = &test_trans2;
+	test_trans1.first_trans = &test_trans1;
+	test_trans2.next = &test_trans3;
+	test_trans2.first_trans = &test_trans1;
+	test_trans3.first_trans = &test_trans1;
+
+	print_block_trans(&test_block, test_string);
+
+	TEST_ASSERT_EQUAL_STRING_LEN("Transaction n.           0000000001\n"
+								 "Sender:                    00005d69\n"
+								 "Receiver:                  008b7100\n"
+								 "Amount:                  0000001340\n"
+								 "Transaction n.           0000000002\n"
+								 "Sender:                    0002ef43\n"
+								 "Receiver:                  0000040f\n"
+								 "Amount:                  0000010923\n"
+								 "Transaction n.           0000000003\n"
+								 "Sender:                    00003021\n"
+								 "Receiver:                  0000266a\n"
+								 "Amount:                  0000000010\n"
+								 "N. di trans.                      3\n",
+								 test_string, TRANS_LENGTH * 3 + LINE_LENGTH + 1);
+}
+
 int main(void) {
 	UNITY_BEGIN();
 	RUN_TEST(test_uint32_to_stringHex_ShouldConvertIntIntoFixedLenString);
@@ -92,5 +160,6 @@ int main(void) {
 	RUN_TEST(test_add_empty_line_ShouldAddEmptyLineToString);
 	RUN_TEST(test_print_block_header_ShouldFormatFixedLenStringWithBlockInfo);
 	RUN_TEST(test_print_trans_ShouldFormatFixedLenStringWithSingleTransInfo);
+	RUN_TEST(test_print_block_trans_ShouldFormatFixedLenStringWithAllBlockTransInfo);
 	return UNITY_END();
 }
