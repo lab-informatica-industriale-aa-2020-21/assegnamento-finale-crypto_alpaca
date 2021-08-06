f4149ac9cf83b5fc92d1d1cb180d76b7e16cfcf6
Emanuele Zanoni <emanuele.zanoni@studenti.unipd.it>
2021-08-06 15:54:46 +0200

    correzione

--- lib/block/src/block.c
+++ lib/block/src/block.c
@@ -64,6 +64,7 @@ struct block *new_block(struct block *const head_block){
 *                 mining per poi essere aggiunto alla chain
 *
 */
+/*
 void mine(struct chain *const chain_to_mine){
     char trans_str [DATA_TRANS * HEX_NUMB_LENGTH * (chain_to_mine -> head_block) -> num_trans + 1];
     format_data_for_hash(chain_to_mine -> head_block, trans_str);
@@ -74,7 +75,7 @@ void mine(struct chain *const chain_to_mine){
     }
 
     (chain_to_mine -> head_block) -> creation_time = time(NULL);      // info mm/gg/yy (data) - h:min:sec (ora) sulla creazione del nuovo blocco
-}
+}*/
 
 
 /*Funzione: get_str_creation_time
