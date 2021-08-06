3ffb31c94c0a4dcab4a8d5ff5219088913c257fa
Alberto Trabacchin <alberto.trabacchin@studenti.unipd.it>
2021-08-06 10:47:35 +0200

    Rimossi file di build

--- build/test/test_block/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/alberto/lab_informatica/assegnamento-finale-crypto_alpaca/test/test_block
-# Build directory: /home/alberto/lab_informatica/assegnamento-finale-crypto_alpaca/build/test/test_block
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(block_test "block_app")
-set_tests_properties(block_test PROPERTIES  _BACKTRACE_TRIPLES "/home/alberto/lab_informatica/assegnamento-finale-crypto_alpaca/test/test_block/CMakeLists.txt;16;add_test;/home/alberto/lab_informatica/assegnamento-finale-crypto_alpaca/test/test_block/CMakeLists.txt;0;")
--- build/test/test_file_IO/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/alberto/lab_informatica/assegnamento-finale-crypto_alpaca/test/test_file_IO
-# Build directory: /home/alberto/lab_informatica/assegnamento-finale-crypto_alpaca/build/test/test_file_IO
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(file_IO_test "file_IO_app")
-set_tests_properties(file_IO_test PROPERTIES  _BACKTRACE_TRIPLES "/home/alberto/lab_informatica/assegnamento-finale-crypto_alpaca/test/test_file_IO/CMakeLists.txt;16;add_test;/home/alberto/lab_informatica/assegnamento-finale-crypto_alpaca/test/test_file_IO/CMakeLists.txt;0;")
