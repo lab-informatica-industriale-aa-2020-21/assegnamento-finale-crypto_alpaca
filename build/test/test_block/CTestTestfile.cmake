2921639bda8e37135ea9f88697df23fcf8e82299
Emanuele Zanoni <emanuele.zanoni@studenti.unipd.it>
2021-08-06 14:55:42 +0200

    corretti alcuni errori

--- build/test/test_block/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/emanuelezanoni/lab_informatica/assegnamento-finale-crypto_alpaca/test/test_block
-# Build directory: /home/emanuelezanoni/lab_informatica/assegnamento-finale-crypto_alpaca/build/test/test_block
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(block_test "block_app")
-set_tests_properties(block_test PROPERTIES  _BACKTRACE_TRIPLES "/home/emanuelezanoni/lab_informatica/assegnamento-finale-crypto_alpaca/test/test_block/CMakeLists.txt;16;add_test;/home/emanuelezanoni/lab_informatica/assegnamento-finale-crypto_alpaca/test/test_block/CMakeLists.txt;0;")
