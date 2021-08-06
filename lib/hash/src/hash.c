04ca9e00517b66a36368d26719807de3d8f4dc1b
Emanuele Zanoni <emanuele.zanoni@studenti.unipd.it>
2021-08-06 15:20:04 +0200

    correzione

--- lib/hash/src/hash.c
+++ lib/hash/src/hash.c
@@ -114,9 +114,9 @@ unsigned int bin_to_decimal (bool *x, int len_x)
     for (int i = len_x - 1; i >= 0 && x[i] == true; i--){
         vett = vett + x[i] * pow(2, 31-i);
     }
-//    for (int i = len_x - 1; i >= 0 && x > 0 ; i--){
-//        vett = vett + x[i] * pow(2, 31-i);
-//    }
+    //    for (int i = len_x - 1; i >= 0 && x > 0 ; i--){
+    //        vett = vett + x[i] * pow(2, 31-i);
+    //    }
 return vett;
 }
 
