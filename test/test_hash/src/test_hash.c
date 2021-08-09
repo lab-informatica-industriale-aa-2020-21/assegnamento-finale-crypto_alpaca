2df2e2febe46409d7cbc9167ac200dadbab65340
Alberto Trabacchin <alberto.trabacchin@studenti.unipd.it>
2021-08-09 15:02:17 +0200

    Aggiunta funzione di test di shift_state_reg del modulo di hash

--- test/test_hash/src/test_hash.c
+++ test/test_hash/src/test_hash.c
@@ -56,10 +56,36 @@ void test_majority_shouldReturnsMajorityBitwiseXYZ(void) {
 	TEST_ASSERT_EQUAL(1842722981, maggiority(3515164733, 1834274982, 1843249093));
 }
 
-void int32_to_char_shouldReturnsStringRepresentingDigitsOfUint32(void) {
+void test_int32_to_char_shouldReturnsStringRepresentingDigitsOfUint32(void) {
 	char exp_array[11] = "1948430201";
 	TEST_ASSERT_EQUAL_MEMORY(exp_array, int_32_to_char(1948430201), 10);
 }
+
+void test_copy_vector_shouldCopyVett1IntoVett2(void) {
+	uint32_t vett1[10] = {1,2,3,4,5,6,7,8,9,0};
+	uint32_t vett2[10];
+	copy_vector(vett1, 10, vett2, 10);
+
+	TEST_ASSERT_EQUAL_UINT32_ARRAY(vett1, vett2, 10);
+}
+
+void test_sum_vector_shouldSaveIntoVett2TheSumOfVett1AndVett2(void) {
+	uint32_t vett1[10] = {1,2,3,4,5,6,7,8,9,0};
+	uint32_t vett2[10] = {0,3,6,2,5,9,8,4,1,7};
+	uint32_t exp_vett[10] = {1,5,9,6,10,15,15,12,10,7};
+	sum_vector(vett1, 10, vett2, 10);
+
+	TEST_ASSERT_EQUAL_UINT32_ARRAY(exp_vett, vett2, 10);
+}
+
+void test_shift_state_reg_shouldRightShiftVettOf1Position(void) {
+	uint32_t vett[10] = {1,2,3,4,5,6,7,8,9,0};
+	uint32_t exp_vett[10] = {1,1,2,3,4,5,6,7,8,9};
+	shift_state_reg(vett, 10);
+
+	TEST_ASSERT_EQUAL_UINT32_ARRAY(exp_vett, vett, 10);
+}
+
 
 int main(void) {
 
@@ -74,10 +100,14 @@ int main(void) {
 
 	RUN_TEST(test_decimal_to_bin_shouldConvertUint32ToBinary);
 	RUN_TEST(test_bin_to_decimal_shouldConvertBinaryToUint32);
-	RUN_TEST(int32_to_char_shouldReturnsStringRepresentingDigitsOfUint32);
+	RUN_TEST(test_int32_to_char_shouldReturnsStringRepresentingDigitsOfUint32);
 
 	RUN_TEST(test_choice_shouldAssignYorXtoZdependingOnX);
 	RUN_TEST(test_majority_shouldReturnsMajorityBitwiseXYZ);
+
+	RUN_TEST(test_copy_vector_shouldCopyVett1IntoVett2);
+	RUN_TEST(test_sum_vector_shouldSaveIntoVett2TheSumOfVett1AndVett2);
+	RUN_TEST(test_shift_state_reg_shouldRightShiftVettOf1Position);
 
 	return(UNITY_END());
 }
