e52711a51710ead2d4c341437ec33a6a1acbea36
Emanuele Zanoni <emanuele.zanoni@studenti.unipd.it>
2021-08-11 17:39:47 +0200

    correzione funzione input_trans

--- lib/blockchain/src/blockchain.c
+++ lib/blockchain/src/blockchain.c
@@ -42,7 +42,7 @@ trans *new_trans(const uint32_t sender, const uint32_t receiver, const uint32_t 
 
     // Aggiunta di una transazione ad una lista esistente:
     else{
-        //tmp_trans -> count_trans = head_trans -> count_trans + 1;
+        tmp_trans -> count_trans = head_trans -> count_trans + 1;
         head_trans -> next_trans = tmp_trans;
     }
     
