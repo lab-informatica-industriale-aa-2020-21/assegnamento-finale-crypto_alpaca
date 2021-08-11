741c992876fd7bda02db275a02a32163af07eb7b
Emanuele Zanoni <emanuele.zanoni@studenti.unipd.it>
2021-08-11 11:41:06 +0200

    prova debug

--- lib/blockchain/src/blockchain.c
+++ lib/blockchain/src/blockchain.c
@@ -37,7 +37,7 @@ trans *new_trans(const uint32_t sender, const uint32_t receiver, const uint32_t 
 
     // Controllo se esiste una lista di transazioni:
     if(head_trans == NULL){
-        //tmp_trans -> count_trans = 0;
+        tmp_trans -> count_trans = 0;
     }
 
     // Aggiunta di una transazione ad una lista esistente:
