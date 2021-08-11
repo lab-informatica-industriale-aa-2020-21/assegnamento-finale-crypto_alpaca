70994f7611225e8118ffba3b021ea625f9900f15
Emanuele Zanoni <emanuele.zanoni@studenti.unipd.it>
2021-08-11 11:37:01 +0200

    prova debug

--- lib/blockchain/src/blockchain.c
+++ lib/blockchain/src/blockchain.c
@@ -80,14 +80,14 @@ block *new_block(block *const head_block){
 
     // Controllo se esiste una lista di blocchi:
     if (head_block == NULL){
-        tmp_block -> count_block = 0;
-        tmp_block -> prev_hash = NULL;
+        //tmp_block -> count_block = 0;
+        //tmp_block -> prev_hash = NULL;
     }
 
     // Aggiunta di un blocco ad una lista esistente:
     else{
-        tmp_block -> count_block = head_block -> count_block +1;
-        tmp_block -> prev_hash = head_block -> hash;
+        //tmp_block -> count_block = head_block -> count_block +1;
+        //tmp_block -> prev_hash = head_block -> hash;
     }
     
     // Inserimento dati del blocco vuoto:
