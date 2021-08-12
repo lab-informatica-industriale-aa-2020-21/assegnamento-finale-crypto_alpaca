ecf9e60c7fe70f219102cffa1e5fdacf122a23ac
Emanuele Zanoni <emanuele.zanoni@studenti.unipd.it>
2021-08-12 16:18:04 +0200

    debug

--- lib/file_IO/src/file_IO.c
+++ lib/file_IO/src/file_IO.c
@@ -92,7 +92,7 @@ uint32_t get_prev_index(const char *file_path){
 
     //posizionamento all'inizio del file
     fseek(fp_chain, 0L, SEEK_SET);
-    if (1){    //controllo di lettora EOF -> true = file vuoto
+    if (feof(fp_chain)){    //controllo di lettora EOF -> true = file vuoto
         printf("funzionaaaaaa");
         return 0;}
 
