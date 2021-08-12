23f13f07e73c7054a851f8489c6be16a52ad4644
Emanuele Zanoni <emanuele.zanoni@studenti.unipd.it>
2021-08-12 15:32:53 +0200

    implementata funzione get_prev_index in input_trans

--- lib/blockchain/src/blockchain.c
+++ lib/blockchain/src/blockchain.c
@@ -199,6 +199,7 @@ void input_trans(uint32_t sender, uint32_t receiver, uint32_t amount, chain *in_
         (in_chain -> head_block) -> head_trans =
             new_trans(sender, receiver, amount, (in_chain -> head_block) -> head_trans);
         (in_chain -> head_block) -> first_trans = (in_chain -> head_block) -> head_trans;
+        (in_chain -> head_block) -> count_block = get_prev_index(BLOCKCHAIN_TXT);
     }
 
     // Controllo se il blocco più recente della chain è già 'minato':
