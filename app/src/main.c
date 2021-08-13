a02b898314c31e4ca1105db7b7e6e0b3f580065d
Filippo Toffano <filippo.toffano@studenti.unipd.it>
2021-08-13 15:41:59 +0200

    definito main e funzione di inserimento manuale

--- app/src/main.c
+++ app/src/main.c
@@ -13,158 +13,139 @@
 #include "blockchain.h"
 #include "file_IO.h"
 
+#define EXIT -1
+#define MANUAL_MODE 0
+#define AUTO_MODE 1
+
+#define ADD_TRANS 0
+#define MINE 1
+#define SHOW_BLOCK 2
+#define RETURN_MAIN_MENU 3
+
+#define YES 1
+#define NO 0
+
+
 // Funzioni modalità di inserimento delle funzioni:
 void manual_trans(chain *head_chain);
 void automatic_trans(chain *head_chain, uint32_t trans_to_generate);
 
 // Funzioni modalità di funzionamento:
-void manual_mode(void);
-void auto_mode(void);
-void end_mode(chain *chain_to_mine);
+void add_chain_manual(chain *blockchain);
+void add_chain_auto(chain *blockchain);
+//void end_mode(chain *chain_to_mine);
 
 
 int main()
 {
-    // Pulizia del terminale 
-    system("clear");
-    
     // Dichiarazioni e iniziallizazione delle variabili:
-    uint8_t work_type = 0;
-    char input_choice;
-    uint8_t mine_choice;
-
+    uint8_t input_choice;
+    chain *chain_1 = new_chain(NULL);
     /*------------------------------------------------------------------------------
     *
     * Introduzione al programma implementato e messaggio di benvenuto
     * Interfacciamento con l'utente
     *
     *--------------------------------------------------------------------------------*/ 
-    printf("\n\nBenvenuto! \n"
-           "Questo programma simula l'algoritmo di gestione di una blockchain"
-           "Sei pronto a scoprirlo? Premi ENTER per continuare...  :)\n");
-        
-    while (getchar()!='\n'){};// attesa di input  'ENTER' da tastiera per continuare l'esecuzione del programma
 
-    do{
-        printf("\n\nE' possibile creare le transazioni oppure fare in modo che il programma le crei in automatico.\n"
-           "Inserisci:\n"
-           "1 -> transazione manuale\n"
-           "2 -> transazione automatica\n");
+    input_choice = selection_box(3, "MANUAL", "AUTOMATIC", "EXIT");
 
-        scanf("%u", &work_type); // lettura scelta di modalità di lavoro 
-        
-        if(work_type != 1 && work_type != 1){
-            printf("\n\nIl valore inserito non è valido!\n"
-                    " Inserisci '0' o '1'");
-        }
+    switch (input_choice)
+    {
+        case MANUAL_MODE:
+            add_chain_manual(chain_1);
+            break;
+        case AUTO_MODE:
+            add_chain_auto(chain_1);
+            break;
+        case EXIT:
+            return 0;
+            break;  
     }
-    while(work_type != 1 && work_type != 2);
+    
 
-    insert_trans: // etichetta per tornare alla creazione delle transazioni
-    
-    if(work_type == 1){
-        manual_mode();
-    }
-    else{
-        auto_mode();
-    }
-    
-    
-   /*-------------------------------------------------------------------------------
-   * Se si decide di terminare il programma, inserendo 'q', i dati creati vengono 
-   * salvati utilizzando la funzione save_chain(chain). Successivamente viene 
-   * deallocata la memoria occupata dalla chain creata richiamando la funzione 
-   * free_chain(chain).
-   * E' possibile trovare la stampa dei dati nel file BLOCKCHAIN_TXT   
-   *--------------------------------------------------------------------------------*/
-    // Richiesta scelta se terminare o continuare il programma 
-   do
-   {
-       while (getchar()!='\n'){};
-        printf("\nSi vuole terminare qui in programma?\n"
-            "Si inserisca:\n"
-            "q -> USCITA/EXIT\n"
-            "c -> Continue\n");
-        scanf("%c", input_choice);
-        // Controllo inserimento carattere corretto per l'uscita o la comtinuazione del programma
-        if(input_choice != 'q' && input_choice != 'Q' && input_choice != 'c' && input_choice != 'C'){
-            printf("\n\nInserisci un carattere valido tra quelli elencati");
-        }
-   } while( input_choice != 'q' && input_choice != 'Q' && input_choice == 'c' && input_choice == 'C');
-   
-    
-    if(input_choice == 'q' || input_choice == 'Q'){ // scelta di terminare
-       end_mode(chain_1);
-    }
-    else if(input_choice == 'c' || input_choice == 'C'){ // scelta di continuare
-        goto insert_trans; 
-    }
 }
 
-/*----------------------------------------------------------------------------------
-**************************** IMPLEMENTAZIONI FUNZIONI ******************************
-------------------------------------------------------------------------------------*/
-
-// Funzione per l'esecuzione del programma manuale
-void manual_mode(void){
-    // MANUAL MODE
-        uint8_t input_choice;
-        chain *chain_1 = new_chain(NULL);
-        printf("\nSi è scelta la modalità di inserimento manuale\n");
-           
-        do{
-            manual_trans(chain_1); // richiamo funzione per l'inserimento manuale delle transazioni
-            while (getchar()!='\n'){};
-            printf("\nVuoi aggiungere un'altra transazone? (Y/N)");
-            scanf("%c", &input_choice);
-            // Controllo inserimento caratteri corretti 
-            while(input_choice != 'Y' && input_choice != 'y' && input_choice != 'N' && input_choice != 'n'){
-                printf("\nL'inserimento non è valido. \n Inserisci uno tra i caratteri indicati");
-                printf("\nVuoi aggiungere un'altra transazone? (Y//N)");
-                while (getchar()!='\n'){};
-                scanf("%c", &input_choice);
-            }
-
-            /*------------------------------------------------------------------------------
-            *  Finita la creazione delle transazioni ,esse vengono aggiunte in un blocco.
-            *  Successivamente e' possibile scegliere se:
-            * 1- il blocco appena creato venga minato e aggiunto alla blockchain;
-            * 2- continuare ad aggiungere transazioni alla stesso blocco.
-            *-------------------------------------------------------------------------------
-            */
-            if(input_choice == 'y' || input_choice == 'Y'){
-                while (getchar()!='\n'){};
-                printf("Vuoi fare il mining del blocco oppure continuare a inserire le transazioni nello stesso blocco?\n"
-                        "Inserisci: \n"
-                        "1- MINING\n"
-                        "2- CONTINUE\n");
-                scanf("%u", &mine_choice);
-
-                while(mine_choice != 2 && mine_choice != 1){
-                    printf("\nL'inserimento non è valido. \n Inserisci uno tra i caratteri indicati");
-                    printf("Inserisci: \n"
-                        "1- MINING\n"
-                        "2- CONTINUE\n");
-                    scanf("%u", &mine_choice);
-                };
-                if(mine_choice == 1){
-                    mine(chain_1);
-                }
-            }
-            
-            // Controllo di conclusione di inserimento delle transazioni 
-            if(input_choice == 'N' || input_choice == 'n'){
-                printf("L'inserimento delle transazioni è terminato!\n"
-                        "Ora il programma precede alla creazione e all'inserimento in un blocco e"
-                        "infine a svolgere l'operazione di mining di quest'ultimo .");
-                mine(chain_1);
-            }
+/*------------------------------------------------------------------------
+* Funzione inserimento transazioni manuale
+* Vengono generati gli identificativi di mittente (sender) e 
+* ricevente (receiver) e anche dell'importo (amount) della transazione
+*-------------------------------------------------------------------------*/ 
+void add_chain_manual(chain *blockchain){
+    uint32_t sender, receiver, amount;
+    uint8_t input_choice;
+    char selection[MAX_ITEMS][MAX_STR_LEN + 1] = {"AGGIUNGI TRANSAZIONE", "MINING", "SHOW BLOCCO", "MENU' PRINCIPALE", "EXIT"};
+    char selection_2 [MAX_ITEMS][MAX_STR_LEN + 1] = {"SI", "NO"};
+    
+    input_choice = selection_box(5, selection);
+    switch (input_choice)
+    {
+    case ADD_TRANS: // aggiunta di una transazione
+        printf("\nInserisci i dati richiesti: \n");
+        printf("\nSender (formato DEC_INT): ");
+        scanf("%u", &sender);
+        printf("\nReceiver (formato DEC_INT): ");
+        scanf("%u", &receiver);
+        printf("\nImporto della transazione (formato DEC_INT): ");
+        scanf("%u", &amount);
+        input_trans(sender, receiver, amount, blockchain); // richiamo funzione per la creazione della transazione
+        break;
+    case MINE: // si mina il blocca
+        mine(blockchain);
+        break;
+    case SHOW_BLOCK: // si stampa il blocco creato
+        save_chain(blockchain, BLOCKCHAIN_TXT);
+        break;
+    case RETURN_MAIN_MENU:
+        // inserire funzione per tornare al menù principale 
+        break;  
+    case EXIT:
+        uint8_t exit_choice;
+        printf("\nSe sicuro di voler uscire dal programma?");
+        exit_choice = selection_box(2, selection_2);
         
-        }while(input_choice == 'y' || input_choice == 'Y');
+        if(exit_choice == YES){
+            free_chain(blockchain);
+            return 0;
+        }
+        else{
+            // torna alla schermata precedente per rifare la selezione
+            input_choice = selection_box(4, selection);
+        }
+        break;
+    }
 }
+    
+
+
+/*------------------------------------------------------------------------
+* Funzione inserimento transazioni automatico
+* Vengono inseriti in maniera casuale gli identificativi del mittente 
+* (sender), destinatario (receiver) e dell'importo della transazioni.
+* Successivamente questi dati vengono passati alla funzione che si occupa
+* della creazione della transazione
+*-------------------------------------------------------------------------*/
+void add_chain_auto(chain *blockchain){
+    uint32_t sender, receiver, amount;
+    uint8_t input_choice;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Funzione per l'esecuzione del programma automatico
-void auto_mode(void){
+void auto_mode(chain *blockchain){
     // ATOMATIC MODE
         chain *chain_1 = new_chain(NULL);
         uint32_t num_trans_to_generate = 0;
@@ -192,35 +173,8 @@ void end_mode(chain *chain_to_mine){
     return 0; // chiusura dell'esecuzione del programma 
 }
 
-/*------------------------------------------------------------------------
-* Funzione inserimento transazioni manuale
-* Vengono generati in modo casuale gli identificativi di mittente (sender) e 
-* e ricevente (receiver) e anche dell'importo (amount) della transazione
-*-------------------------------------------------------------------------*/ 
-void manual_trans(chain *head_chain)
-{
-    uint32_t sender, receiver, amount;
-    
-    printf("\n");
-    printf("Inserisci i dati richiesti: \n");
-    printf("\nSender (formato DEC_INT): ");
-    scanf("%u", &sender);
-    printf("\nReceiver (formato DEC_INT): ");
-    scanf("%u", &receiver);
-    printf("\nImporto della transazione (formato DEC_INT): ");
-    scanf("%u", &amount);
-    while (getchar()!='\n');
-    input_trans(sender, receiver, amount, head_chain); // richiamo funzione per la creazione della transazione
-}
 
 
-/*------------------------------------------------------------------------
-* Funzione inserimento transazioni automatico
-* Vengono inseriti in maniera casuale gli identificativi del mittente 
-* (sender), destinatario (receiver) e dell'importo della transazioni.
-* Successivamente questi dati vengono passati alla funzione che si occupa
-* della creazione della transazione
-*-------------------------------------------------------------------------*/
 void automatic_trans(chain *head_chain, uint32_t trans_to_generate)
 {
     uint32_t sender, receiver, amount;
@@ -230,19 +184,11 @@ void automatic_trans(chain *head_chain, uint32_t trans_to_generate)
     * al mittente e al destinatario.
     * Il numero max assegnabile corrisponde a 2^32 = 4.294.967.296
     * Eseguendo 4*rand()*rand() il numero max raggiungibile è 4.294.705.156,
-    * ovvero 4 * 32767 * 32767 (max numero raggiungibile in c)
+    * ovvero 4 * 32767 * 32767 (max numero raggiungibile in C)
     * L'importo può variare tra 1€ e 3276700€ (32767*100)
     * 
     * rand(): funzione che genera numeri random contenuta nella libreria 
     * standard <stdlib.h>
     *---------------------------------------------------------------------*/
 
-    for(uint32_t i = 0; i < trans_to_generate; i++){
-        
-        sender = 4 * rand() * rand(); // assegnazione identificativo casuale al mittente (sender)
-        receiver = 4 * rand() * rand(); // assegnazione identificativo casuale al destinatario (receiver)
-        amount = 1 + rand() * 100; // assegnazione di importo (amount) causale
-
-        input_trans(sender, receiver, amount, head_chain); // richiamo funzione per la creazione della transazione
-        }
 }
