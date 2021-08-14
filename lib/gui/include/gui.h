1fb67d204f3cac3fd10a36e5db6deece4a33105b
Emanuele Zanoni <emanuele.zanoni@studenti.unipd.it>
2021-08-14 15:59:53 +0200

    la funzione selection_box stampa anche il titolo

--- lib/gui/include/gui.h
+++ lib/gui/include/gui.h
@@ -5,9 +5,28 @@
 
 #define MAX_ROWS 24
 #define MAX_COLS 80
-#define MAX_STR_LEN (MAX_COLS - 6)
-#define MAX_ITEMS (MAX_ROWS - 2 - 1)
+#define MAX_STR_LEN (MAX_COLS - 4)
+#define MAX_ITEMS (MAX_ROWS - 2 - 3)
+#define UNUSABLE_ROWS 3
 
-int selection_box(int n_items, char selections [MAX_ITEMS][MAX_STR_LEN + 1]);
+//colori
+#define TITLE_COLOR 1
+#define SELECT_COLOR 2
+#define NON_SELECT_COLOR 3
+#define QUIT_COLOR 4
+
+/*
+ * colori disponibili:
+ * COLOR_BLACK
+ * COLOR_RED
+ * COLOR_GREEN
+ * COLOR_YELLOW
+ * COLOR_BLUE
+ * COLOR_MAGENTA
+ * COLOR_CYAN
+ * COLOR_WHITE
+ */
+
+int selection_box(char *title, int n_items, char selections [MAX_ITEMS][MAX_STR_LEN + 1]);
 
 #endif
--- lib/gui/src/gui.c
+++ lib/gui/src/gui.c
@@ -3,22 +3,40 @@
 
 #include "gui.h"
 
-int selection_box(int num_items, char selections [MAX_ITEMS][MAX_STR_LEN + 1]){
+int selection_box(char *title,int num_items, char selections [MAX_ITEMS][MAX_STR_LEN + 1]){
     char item[MAX_STR_LEN + 1];
     int n_items;
     int input = 0;
     int tmp = 0;
 
+    //controllo num_items entro il limite massimo
     if (num_items > MAX_ITEMS)
         n_items = MAX_ITEMS;
     else
         n_items = num_items;
 
+    //inizializzazione
     initscr();
+
+    //inizializzazione colori
+    if (has_colors()){
+        start_color();
+        init_pair(TITLE_COLOR, COLOR_GREEN, COLOR_BLACK);
+    }
+
+    //inizializzazione finestra
     WINDOW *w;
     w = newwin(MAX_ROWS, MAX_COLS, 0, 0);
     box(w, 0, 0);
 
+    //stampa titolo
+    wattron(w, A_BOLD | A_UNDERLINE);
+    sprintf(item, "%-*s", (int)(sizeof(title) - sizeof(char)), title);
+    mvwprintw(w, 1, 2, "%s", item);
+    wattrset(w, A_NORMAL);
+
+
+    //stampa selezioni
     for (int i = 0; i < n_items; i++){
         if (i == 0)
             wattron(w, A_STANDOUT);
@@ -26,30 +44,33 @@ int selection_box(int num_items, char selections [MAX_ITEMS][MAX_STR_LEN + 1]){
             wattroff(w, A_STANDOUT);
 
         sprintf(item, "%-*s", MAX_STR_LEN, selections[i]);
-        mvwprintw(w, i + 1, 2, "%s", item);
+        mvwprintw(w, i + UNUSABLE_ROWS, 2, "%s", item);
+        wattrset(w, A_NORMAL);
     }
 
+    //stampa "QUIT"
     sprintf(item, "%-*s", MAX_STR_LEN, "QUIT");
-    mvwprintw(w, MAX_ITEMS + 1, 2, "%s", item);
+    mvwprintw(w, MAX_ITEMS + UNUSABLE_ROWS, 2, "%s", item);
 
     wrefresh(w);
 
+    //impostazioni visualizzazione finestra
     noecho();
     keypad(w, TRUE);
     curs_set(0);
 
-
+    //ciclo while di selezione
     while((input = wgetch(w)) != '\n'){
         if(tmp != n_items){
             sprintf(item, "%-*s", MAX_STR_LEN, selections[tmp]);
-            mvwprintw(w, tmp + 1, 2, "%s", item);
-            wattron(w, A_STANDOUT);
+            mvwprintw(w, tmp + UNUSABLE_ROWS, 2, "%s", item);
         }
         else{
             sprintf(item, "%-*s", MAX_STR_LEN, "QUIT");
-            mvwprintw(w, MAX_ITEMS + 1, 2, "%s", item);
-            wattron(w, A_STANDOUT);
+            mvwprintw(w, MAX_ITEMS + UNUSABLE_ROWS, 2, "%s", item);
         }
+
+        wattron(w, A_STANDOUT);
 
         switch(input){
             case KEY_UP:
@@ -65,19 +86,21 @@ int selection_box(int num_items, char selections [MAX_ITEMS][MAX_STR_LEN + 1]){
 
         if(tmp != n_items){
             sprintf(item, "%-*s", MAX_STR_LEN, selections[tmp]);
-            mvwprintw(w, tmp + 1, 2, "%s", item);
-            wattroff(w, A_STANDOUT);
+            mvwprintw(w, tmp + UNUSABLE_ROWS, 2, "%s", item);
         }
         else{
             sprintf(item, "%-*s", MAX_STR_LEN, "QUIT");
-            mvwprintw(w, MAX_ITEMS + 1, 2, "%s", item);
-            wattroff(w, A_STANDOUT);
+            mvwprintw(w, MAX_ITEMS + UNUSABLE_ROWS, 2, "%s", item);
         }
+
+        wattroff(w, A_STANDOUT);
     }
 
+    //chiusura finestra
     delwin(w);
     endwin();
 
+    //return
     if(tmp == n_items)
         return -1;
     else
